41c64e15c3d074637a936563aec9f3eb93d6c9e8
Group project <loverboylakshman7@gmail.com>
2026-07-10 17:48:01 +0530

    Update notifications.js

--- js/notifications.js
+++ js/notifications.js
@@ -1,1 +1,23 @@
-alert("Student Added");
+function showNotifications(){
+
+let messages=[
+
+"New student registered",
+
+"Attendance updated",
+
+"Monthly report generated"
+
+];
+
+let text="Notifications\n\n";
+
+messages.forEach((msg,index)=>{
+
+text+=(index+1)+". "+msg+"\n";
+
+});
+
+alert(text);
+
+}
